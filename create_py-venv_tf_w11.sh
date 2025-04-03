bbaa574febe6ff1621d78a6a48101d6d1d63a018
Pol Suárez <polsm@mech.kth.se>
2025-04-03 19:58:22 +0200

    new sh to ubuntu

--- create_py-venv_tf_w11.sh
+++ create_py-venv_tf_w11.sh
@@ -2,16 +2,16 @@
 set -ex  # Verbose and stop on error
 
 # Create virtual environment with Python 3.10
-py -3.10 -m venv DRL-tf-w11
+py -3.10 -m venv DRL-tf-ubuntu
 
 # Activate the virtual environment (Git Bash style)
-source ./DRL-tf-w11/Scripts/activate
+source ./DRL-tf-ubuntu/bin/activate
 
 # Upgrade pip using full path (Windows quirk fix)
-./DRL-tf-w11/Scripts/python.exe -m pip install --upgrade pip
+pip install --upgrade pip
 
 # Install dependencies
-pip install -r requirements_venv_tf_w11.txt
+#pip install -r requirements_venv_tf_w11.txt
 
 # Show what's installed
 pip list
