dcd0d8911b26a288f3f1b596d39e9e4ef8703d3e
polsuarezm <polsuarezm@gmail.com>
2025-04-04 12:05:44 +0200

    venv ready for ubuntu

---
+++ create_py-venv_tf_ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#set -ex  # Verbose and stop on error
+
+sudo apt update
+sudo apt install -y build-essential curl wget git unzip zip \
+    software-properties-common ca-certificates apt-transport-https \
+    gnupg lsb-release
+
+sudo apt install -y python3 python3-venv python3-pip
+
+# Create virtual environment with Python 3.10
+python3 -m venv pyenv-DRL-tf
+
+# Activate the virtual environment (Git Bash style)
+source pyenv-DRL-tf/bin/activate
+
+# Upgrade pip using full path (Windows quirk fix)
+pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements_venv_tf_w11.txt
+
+# Show what's installed
+pip list
