a438c4be9b2695a898cc325484c4a1382e8ba1d8
polsuarezm <polsuarezm@gmail.com>
2025-07-03 12:19:11 +0200

    -m

--- push_git_watzmann.sh
+++ push_git_watzmann.sh
@@ -1,5 +1,19 @@
-#eval "$(ssh-agent -s)"
-ssh-add ssh_watzmann_git 
+#!/bin/bash
 
+# Check if commit message was passed
+if [ -m "$1" ]; then
+  echo "Usage: $0 \"commit message\""
+  exit 1
+fi
+
+# Start SSH agent
+eval "$(ssh-agent -s)"
+
+# Add private key
+ssh-add ssh_watzzman_git
+
+# Git operations
+git add .
+git commit -m "$1"
+git remote set-url origin git@github.com:polsuarezm/AFC-DRL-experiment.git
 git push origin main
-# passphrase: polete
